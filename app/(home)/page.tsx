d80ed4d9159d7af41584b0e5c6e661ae06ea0428
vrpst <lucas.verpoest@gmail.com>
2025-11-01 15:35:17 +0000

    move board init to server

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,25 @@
-'use client'
-
-import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
+import { redirect } from 'next/navigation';
+import React from 'react'
+import WebSocket, { WebSocketServer } from "ws";
 
 const Page = () => {
 
     // this code is temporary - redirect user to random uuid
-    const router = useRouter();
     const uuid = crypto.randomUUID();
 
-    useEffect(() => {
-        router.push(`/board/${uuid}`);
-    }, [])
+
+        // socket stuff
+        /*const wss = new WebSocketServer({ port: 8080 });
+        wss.on("connection", (ws) => {
+        console.log("Client connected");
+        ws.on("message", (message) => {
+            console.log("Received:", message);
+            ws.send(`Echo: ${message}`);
+        });
+        });*/
+
+        // uuid
+    redirect(`/board/${uuid}`);
 
     return (
         <div className='w-screen h-screen'>
